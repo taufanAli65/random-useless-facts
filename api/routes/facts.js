373ea3a214b22224e5210fa44a74a9717490f735
taufanAli65 <taufanali65@gmail.com>
2025-05-18 10:15:50 +0700

    fix: Correct route definitions for fetching facts

--- api/routes/facts.js
+++ api/routes/facts.js
@@ -8,8 +8,8 @@ const { getFactHandler,
     deleteFactHandler } = require("../controllers/facts");
 
 router.get("/:id", async (req, res) => { getFactHandler(req, res) });
-router.get("/", async (req, res) => { getFactsHandler(req, res) });
-router.get("/random", async (req, res) => { getRandomFactHandler(req, res) });
+router.get("/all", async (req, res) => { getFactsHandler(req, res) });
+router.get("/", async (req, res) => { getRandomFactHandler(req, res) });
 router.post("/", async (req, res) => { addFactHandler(req, res) });
 router.put("/:id", async (req, res) => { updateFactHandler(req, res) });
 router.delete("/:id", async (req, res) => { deleteFactHandler(req, res) });
